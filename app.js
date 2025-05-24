5520880d75df60d4dbf0660cccf5b3ce232fbf30
rishi7773 <rishishekhawat381@gmail.com>
2025-05-24 10:14:40 +0530

    Updated Hello World message in app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello, World ");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(3005, () => {
+    console.log('Server running at http://localhost:3005/');
+});
