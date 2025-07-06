acdc28b41ff248a850b2b15bcc6fceb2e1cbcd57
rishi7773 <rishishekhawat381@gmail.com>
2025-07-06 23:57:50 +0530

    Assignment 7: JWT authentication with protected route

--- assignment-7-week7/index.js
+++ assignment-7-week7/index.js
@@ -12,7 +12,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
 
 mongoose
-    .connect("mongodb+srv://appicleanimation:rishi420@cluster0.cjztx.mongodb.net/crud?retryWrites=true&w=majority")
+    .connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
     .then(() => {
         console.log('MongoDB connected');
         app.listen(process.env.PORT, () =>
